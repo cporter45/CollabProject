1deacce771bc692c03e5971b3a58bba464bb928b
Chase P <40186786+cporter45@users.noreply.github.com>
2023-08-12 16:49:12 -0500

    movement
    Add movement

---
+++ Assets/player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+    }
+}
